f278793bfee750283178469e3cbda6334765547d
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-05-23 18:38:20 +0300

    80

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -7,22 +7,46 @@
 #include <stdexcept>
 #include <string>
 
+
 namespace ariel {
     OrgChart::OrgChart() {
-        this->root=NULL;
     }
 
     OrgChart::~OrgChart() {}
 
 
     OrgChart &OrgChart::add_root(string s) {
-        this->root.value=s;
+        this->root.value = s;
         return *this;
     }
 
     OrgChart &OrgChart::add_sub(string s1, string s2) {
+        bool b = add_sub_check(s1, s2, &root);
+        if (b) {
+            return *this;
+        }
+        throw std::invalid_argument("There is no such Node 'root' in the OrgChart");
+//        return *this;
+    }
 
-        return *this;
+    bool OrgChart::add_sub_check(string s1, string s2, node *root) {
+        bool x = false;
+        if (root->value == s1) {
+            node *new_node = new node();
+            new_node->value = s2;
+            root->sons.push_back(new_node);
+            return true;
+        }
+        vector <node*> sons_of_root = root->sons;
+        if (!sons_of_root.empty()){
+            for (node* s: sons_of_root){
+                x= add_sub_check(s1,s2,s);
+                if (x==true){
+                    return true;
+                }
+            }
+        }
+        return x;
     }
 
     std::ostream &operator<<(std::ostream &output, const OrgChart &o) {
--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -10,23 +10,23 @@
 #include <list>
 #include <queue>
 #include <stack>
+#include <map>
 
 #pragma once
-#ifndef CPP_EX5_B_ORGCHART_H
-#define CPP_EX5_B_ORGCHART_H
+#ifndef CPP_EX5_B_VER_2_ORGCHART_H
+#define CPP_EX5_B_VER_2_ORGCHART_H
 
-#endif //CPP_EX5_B_ORGCHART_H
+#endif //CPP_EX5_B_VER_2_ORGCHART_H
 using namespace std;
 namespace ariel {
     class OrgChart {
     public:
         typedef struct node {
             string value;
-            node* brother;
-            node* son;
+            vector<node *> sons;
         } node;
 
-        node *root;
+        node root;
 
         OrgChart();
 
@@ -36,88 +36,94 @@ namespace ariel {
 
         OrgChart &add_sub(string s1, string s2);
 
+        bool add_sub_check(string s1, string s2, node *root);
+
         friend std::ostream &operator<<(std::ostream &output, const OrgChart &o);
 
         class level_order_Iterator {
         public:
-            queue<node*>brothers;
-            queue<node*>sons;
+            queue<node *> sons;
             node *_ptr;
 
+
             level_order_Iterator(node *ptr) : _ptr(ptr) {
-                this->brothers.push(ptr);
+                this->sons.push(ptr);
             }
 
             string &operator*() {
                 return _ptr->value;
             }
-            node* operator&(){
+
+            node *operator&() {
                 return _ptr;
             }
 
-            string& operator*() const {
+            string &operator*() const {
                 return _ptr->value;
             }
 
-            string* operator->() const {
+            string *operator->() const {
                 return &(_ptr->value);
             }
+
             level_order_Iterator &operator++() {
-                node* bro=this->brothers.front()->brother;
-                node* son=this->sons.front()->son;
-                if (bro!= nullptr){
-                    brothers.push(bro);
+                node *curr = this->sons.front();
+                for (size_t i = 0; i < curr->sons.size(); ++i) {
+                    this->sons.push(curr->sons.at(i));
                 }
-                if(son!= nullptr){
-                    sons.push(son);
+                if (this->sons.size() == 1 && sons.front()->sons.empty()) {
+                    this->_ptr = nullptr;
+                } else {
+                    this->sons.pop();
+                    this->_ptr = sons.front();
                 }
-                brothers.pop();
-                if (brothers.empty()){
-                    node* n=sons.front();
-                    sons.pop();
-                    brothers.push(n);
-                }
-                _ptr=brothers.front();
                 return *this;
             }
+
+
             const level_order_Iterator operator++(int) {
-                level_order_Iterator tmp= *this;
-                node* bro=this->brothers.front()->brother;
-                node* son=this->sons.front()->son;
-                if (bro!= nullptr){
-                    brothers.push(bro);
+                level_order_Iterator tmp = *this;
+                node *curr = this->sons.front();
+                for (size_t i = 0; i < curr->sons.size(); ++i) {
+                    this->sons.push(curr->sons.at(i));
                 }
-                if(son!= nullptr){
-                    sons.push(son);
+                if (this->sons.size() == 1 && sons.front()->sons.empty()) {
+                    this->_ptr = nullptr;
+                } else {
+                    this->sons.pop();
+                    this->_ptr = sons.front();
                 }
-                brothers.pop();
-                if (brothers.empty()){
-                    node* n=sons.front();
-                    sons.pop();
-                    brothers.push(n);
-                }
-                _ptr=brothers.front();
                 return tmp;
             }
-            bool operator==(const level_order_Iterator& it) const {
+
+            bool operator==(const level_order_Iterator &it) const {
                 return _ptr == it._ptr;
             }
 
-            bool operator!=(const level_order_Iterator& it) const {
+            bool operator!=(const level_order_Iterator &it) const {
                 return _ptr != it._ptr;
             }
         };
-        level_order_Iterator begin_level_order(){
-            return level_order_Iterator{root};
+
+        level_order_Iterator begin_level_order() {
+            if (root.value != "") {
+                return level_order_Iterator{&root};
+            }
+            throw logic_error("root is null");
         }
-        level_order_Iterator end_level_order(){
-            return nullptr;
+
+        level_order_Iterator end_level_order() {
+            if (root.value != "") {
+                return nullptr;
+            }
+            throw logic_error("root is null");
         }
+
         class preorder_Iterator {
         public:
+            stack<node *> sons;
             node *_ptr;
-            stack<node*>brothers;
-            queue<node*>sons;
+
 
             preorder_Iterator(node *ptr) : _ptr(ptr) {
                 this->sons.push(ptr);
@@ -127,139 +133,166 @@ namespace ariel {
                 return _ptr->value;
             }
 
-            node* operator&(){
+            node *operator&() {
                 return _ptr;
             }
 
-            string& operator*() const {
+            string &operator*() const {
                 return _ptr->value;
             }
 
-            string* operator->() const {
+            string *operator->() const {
                 return &(_ptr->value);
             }
+
             preorder_Iterator &operator++() {
-                node* bro=this->brothers.top()->brother;
-                node* son=this->sons.front()->son;
-                if (bro!= nullptr){
-                    brothers.push(bro);
+                node *curr = this->sons.top();
+                this->sons.pop();
+                for (int i = (int) curr->sons.size() - 1; i >= 0; --i) {
+                    this->sons.push(curr->sons.at((unsigned int) i));
                 }
-                if(son!= nullptr){
-                    sons.push(son);
+                if (this->sons.empty()) {
+                    this->_ptr = nullptr;
+                } else {
+                    this->_ptr = sons.top();
                 }
-                sons.pop();
-                if (sons.empty()){
-                    node* n=brothers.top();
-                    sons.pop();
-                    brothers.push(n);
-                }
-                _ptr=sons.front();
                 return *this;
             }
+
+
             const preorder_Iterator operator++(int) {
-                preorder_Iterator tmp= *this;
-                node* bro=this->brothers.top()->brother;
-                node* son=this->sons.front()->son;
-                if (bro!= nullptr){
-                    brothers.push(bro);
+                preorder_Iterator tmp = *this;
+                node *curr = this->sons.top();
+                this->sons.pop();
+                for (int i = (int) curr->sons.size() - 1; i >= 0; --i) {
+                    this->sons.push(curr->sons.at((unsigned int) i));
                 }
-                if(son!= nullptr){
-                    sons.push(son);
+                if (this->sons.empty()) {
+                    this->_ptr = nullptr;
+                } else {
+                    this->_ptr = sons.top();
                 }
-                sons.pop();
-                if (sons.empty()){
-                    node* n=brothers.top();
-                    sons.pop();
-                    brothers.push(n);
-                }
-                _ptr=sons.front();
                 return tmp;
             }
-            bool operator==(const preorder_Iterator& it) const {
+
+            bool operator==(const preorder_Iterator &it) const {
                 return _ptr == it._ptr;
             }
 
-            bool operator!=(const preorder_Iterator& it) const {
+            bool operator!=(const preorder_Iterator &it) const {
                 return _ptr != it._ptr;
             }
         };
-        preorder_Iterator begin_preorder(){
-            return preorder_Iterator{root};
+
+        preorder_Iterator begin_preorder() {
+            if (root.value != "") {
+                return preorder_Iterator{&root};
+            }
+            throw logic_error("root is null");
         }
-        preorder_Iterator end_preorder(){
+
+        preorder_Iterator end_preorder() {
+            if (root.value != "") {
+                return nullptr;
+            }
+            throw logic_error("root is null");
+        }
+
+        class reverse_order_Iterator {
+        public:
+            vector<node *> sons;
+            queue<node *> my_queue;
+            node *_ptr;
+            int index;
+
+
+            reverse_order_Iterator(node *ptr) : _ptr(ptr) {
+                if (ptr == nullptr) {
+                    return;
+                }
+                node *curr = ptr;
+                my_queue.push(curr);
+                while (!my_queue.empty()) {
+                    for (int i = (int) my_queue.front()->sons.size() - 1; i >= 0; --i) {
+                        this->my_queue.push(my_queue.front()->sons.at((unsigned int) i));
+                    }
+                    node *f = my_queue.front();
+                    my_queue.pop();
+                    sons.push_back(f);
+                }
+                index = sons.size() - 1;
+                _ptr = this->sons.at((unsigned int) index);
+            }
+
+            string &operator*() {
+                return _ptr->value;
+            }
+
+            node *operator&() {
+                return _ptr;
+            }
+
+            string &operator*() const {
+                return _ptr->value;
+            }
+
+            string *operator->() const {
+                return &(_ptr->value);
+            }
+
+            reverse_order_Iterator &operator++() {
+                index--;
+                try {
+                    _ptr = this->sons.at((unsigned int) index);
+                }
+                catch (const std::exception &e) {
+                    _ptr = nullptr;
+                }
+                return *this;
+            }
+
+
+            const reverse_order_Iterator operator++(int) {
+                reverse_order_Iterator tmp = *this;
+                index--;
+                try {
+                    _ptr = this->sons.at((unsigned int) index);
+                }
+                catch (const std::exception &e) {
+                    _ptr = nullptr;
+                }
+                return tmp;
+            }
+
+            bool operator==(const reverse_order_Iterator &it) const {
+                return _ptr == it._ptr;
+            }
+
+            bool operator!=(const reverse_order_Iterator &it) const {
+                return _ptr != it._ptr;
+            }
+        };
+
+        reverse_order_Iterator begin_reverse_order() {
+            if (root.value != "") {
+                return reverse_order_Iterator{&root};
+            }
+            throw logic_error("root is null");
+        }
+
+        reverse_order_Iterator reverse_order() {
+            if (root.value != "") {
+                return reverse_order_Iterator{nullptr};
+            }
+            throw logic_error("root is null");
+        }
+
+        level_order_Iterator begin() {
+            return level_order_Iterator{&root};
+        }
+
+        level_order_Iterator end() {
             return nullptr;
         }
-
-        preorder_Iterator(node *ptr) : _ptr(ptr) {
-                this->sons.push(ptr);
-        }
-
-        string &operator*() {
-            return _ptr->value;
-        }
-
-        node* operator&(){
-            return _ptr;
-        }
-
-        string& operator*() const {
-            return _ptr->value;
-        }
-
-        string* operator->() const {
-            return &(_ptr->value);
-        }
-        preorder_Iterator &operator++() {
-            node* bro=this->brothers.top()->brother;
-            node* son=this->sons.front()->son;
-            if (bro!= nullptr){
-                brothers.push(bro);
-            }
-            if(son!= nullptr){
-                sons.push(son);
-            }
-            sons.pop();
-            if (sons.empty()){
-                node* n=brothers.top();
-                sons.pop();
-                brothers.push(n);
-            }
-            _ptr=sons.front();
-            return *this;
-        }
-        const preorder_Iterator operator++(int) {
-            preorder_Iterator tmp= *this;
-            node* bro=this->brothers.top()->brother;
-            node* son=this->sons.front()->son;
-            if (bro!= nullptr){
-                brothers.push(bro);
-            }
-            if(son!= nullptr){
-                sons.push(son);
-            }
-            sons.pop();
-            if (sons.empty()){
-                node* n=brothers.top();
-                sons.pop();
-                brothers.push(n);
-            }
-            _ptr=sons.front();
-            return tmp;
-        }
-        bool operator==(const preorder_Iterator& it) const {
-            return _ptr == it._ptr;
-        }
-
-        bool operator!=(const preorder_Iterator& it) const {
-            return _ptr != it._ptr;
-        }
-    };
-    preorder_Iterator begin_preorder(){
-        return preorder_Iterator{root};
-    }
-    preorder_Iterator end_preorder(){
-        return nullptr;
-    }
-
     };
 }
